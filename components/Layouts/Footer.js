2466f05e37214a8da6788010d1658d80460642a5
vahanmkrtumyan <vahanmkrtumyan@gmail.com>
2025-03-30 22:19:08 +0400

    Changed footer

--- components/Layouts/Footer.js
+++ components/Layouts/Footer.js
@@ -1,6 +1,5 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import TechhubLogoWhite from '../Services/LogoWhite';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -9,51 +8,15 @@ const Footer = () => {
       <footer className="footer-area pt-100 pb-70 jarallax">
         <div className="container">
           <div className="row">
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="single-footer-widget">
-                <Link href="/" className="logo">
-                  <Image
-                    src="/images/logo.png"
-                    alt="Image"
-                    width={150}
-                    height={42}
-                  />
-                </Link>
-
-                <p>
-                  Lorem ipsum dolor sit amet, consec tetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
-                  consec tetur adipiscing.
-                </p>
-
-                <ul className="social-icon">
-                  <li>
-                    <a href="https://www.facebook.com/" target="_blank">
-                      <i className="bx bxl-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.instagram.com/" target="_blank">
-                      <i className="bx bxl-instagram"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.linkedin.com/" target="_blank">
-                      <i className="bx bxl-linkedin-square"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://twitter.com/" target="_blank">
-                      <i className="bx bxl-twitter"></i>
-                    </a>
-                  </li>
-                </ul>
+                <TechhubLogoWhite />
               </div>
             </div>
 
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="single-footer-widget">
-                <h3>Address</h3>
+                <h3>Հասցե</h3>
 
                 <ul className="address">
                   <li className="location">
@@ -76,57 +39,19 @@ const Footer = () => {
               </div>
             </div>
 
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="single-footer-widget">
-                <h3>Solution</h3>
+                <h3>Հղումներ</h3>
 
                 <ul className="import-link">
                   <li>
-                    <Link href="/solutions/details">Secure Private Cloud</Link>
+                    <Link href="#about">ՄԵր մասին</Link>
                   </li>
                   <li>
-                    <Link href="/solutions/details">Online Backup</Link>
+                    <Link href="#services">Ծառայություններ</Link>
                   </li>
                   <li>
-                    <Link href="/solutions/details">Secure Websites</Link>
-                  </li>
-                  <li>
-                    <Link href="/solutions/details">Cyber-Security</Link>
-                  </li>
-                  <li>
-                    <Link href="/solutions/details">Enterprise Networks</Link>
-                  </li>
-                  <li>
-                    <Link href="/solutions/details">Collaboration</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="col-lg-3 col-md-6">
-              <div className="single-footer-widget">
-                <h3>Resources</h3>
-
-                <ul className="import-link">
-                  <li>
-                    <Link href="/services/details">
-                      Cybersecurity Strategies
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/services/details">Security Advisories</Link>
-                  </li>
-                  <li>
-                    <Link href="/services/details">Security News</Link>
-                  </li>
-                  <li>
-                    <Link href="/blog">Blog</Link>
-                  </li>
-                  <li>
-                    <Link href="/solutions">Case Studies</Link>
-                  </li>
-                  <li>
-                    <Link href="/contact">User Guides</Link>
+                    <Link href="#contact">Կապ</Link>
                   </li>
                 </ul>
               </div>
@@ -134,32 +59,6 @@ const Footer = () => {
           </div>
         </div>
       </footer>
-
-      <div className="copy-right-area">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-lg-6 col-md-6">
-              <p>
-                Copyright &copy; {currentYear} Nezox. Designed{" "}
-                <a href="https://envytheme.com/" target="_blank">
-                  EnvyTheme
-                </a>
-              </p>
-            </div>
-
-            <div className="col-lg-6 col-md-6">
-              <ul className="footer-menu">
-                <li>
-                  <Link href="/privacy-policy">Privacy Policy</Link>
-                </li>
-                <li>
-                  <Link href="/terms-conditions">Terms & Conditions</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 };
